fdb6876fcc2df0826129ec72f5387c25ce72fc80
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-01-02 11:41:28 +0700

    slicing checkout n register success

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,4 +15,8 @@ class CartController extends Controller
     public function index(){
         return view('pages.cart');
     }
+
+    public function success(){
+        return view('pages.success');
+    }
 }
