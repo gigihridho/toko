54dc336fb3a8bfec4013896d7ed7fe7eb1e678cb
gigihridho <gigih.r.r@mail.ugm.ac.id>
2021-01-05 10:04:47 +0700

    crud gallery

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,8 +30,11 @@
                       <a href="{{ route('admin-dashboard') }}" class="list-group-item list-group-item-action">
                           Dashboard
                       </a>
-                      <a href="{{ route('product.index') }}" class="list-group-item list-group-item-action" {{ (request()->is('admin/product*')) ? 'active' : '' }}>
+                      <a href="{{ route('product.index') }}" class="list-group-item list-group-item-action" {{ (request()->is('admin/product')) ? 'active' : '' }}>
                         Product
+                      </a>
+                      <a href="{{ route('product-gallery.index') }}" class="list-group-item list-group-item-action" {{ (request()->is('admin/product-gallery*')) ? 'active' : '' }}>
+                        Product Gallery
                       </a>
                       <a href="{{ route('category.index') }}" class="list-group-item list-group-item-action {{ (request()->is('admin/category*')) ? 'active' : '' }}">
                         Categories
